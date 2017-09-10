556f1ee711167c8b91bd6a255c0d792bf8e553bb
BartlomiejRasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2017-09-10 21:15:30 +0200

    Small fixes

--- app/src/prod/java/pl/rasztabiga/architecturecomponents/Injection.java
+++ app/src/prod/java/pl/rasztabiga/architecturecomponents/Injection.java
@@ -12,7 +12,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
 public class Injection {
 
     private Injection() {
-        
+
     }
 
     public static BooksRepository provideBooksRepository(@NonNull Context context) {
