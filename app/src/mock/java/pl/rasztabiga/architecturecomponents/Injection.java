01ded92664890d07fdc3bb0ddfeee38a3739d56e
BartlomiejRasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2017-09-11 22:08:07 +0200

    Fix mock Injection

--- app/src/mock/java/pl/rasztabiga/architecturecomponents/Injection.java
+++ app/src/mock/java/pl/rasztabiga/architecturecomponents/Injection.java
@@ -3,19 +3,23 @@ package pl.rasztabiga.architecturecomponents;
 import android.content.Context;
 import android.support.annotation.NonNull;
 
-import pl.rasztabiga.architecturecomponents.books.BooksLocalDataSource;
-import pl.rasztabiga.architecturecomponents.books.BooksRemoteDataSource;
 import pl.rasztabiga.architecturecomponents.books.BooksRepository;
+import pl.rasztabiga.architecturecomponents.books.persistence.BooksDatabase;
+import pl.rasztabiga.architecturecomponents.books.persistence.BooksLocalDataSource;
+import pl.rasztabiga.architecturecomponents.books.persistence.BooksRemoteDataSource;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
 public class Injection {
 
-    private Injection() {}
+    private Injection() {
+
+    }
 
     public static BooksRepository provideBooksRepository(@NonNull Context context) {
         checkNotNull(context);
+        BooksDatabase database = BooksDatabase.getInstance(context);
         return BooksRepository.getInstance(BooksRemoteDataSource.getInstance(),
-                BooksLocalDataSource.getInstance());
+                BooksLocalDataSource.getInstance(database.booksDao()));
     }
 }
